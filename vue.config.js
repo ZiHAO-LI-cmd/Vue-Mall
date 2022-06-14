c1bede286ad8a4a59e2b936590bbe64a95b0a352
Zihao <zihao-lee@outlook.com>
2022-06-14 20:15:58 +0800

    first

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 关闭eslint
+  lintOnSave: false,
+
+  devServer: {
+    // disableHostCheck: false,
+    host: 'localhost',
+    port: 8081,
+    https: false,
+    hot: false,
+    proxy: null
+  },
 })
