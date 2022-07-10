7107d257f720c4304411a95a48f51c7cd914fe85
Zihao <zihao-lee@outlook.com>
2022-07-11 02:04:54 +0800

    动态展示三级联动数据

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,8 +1,25 @@
-// home模块小仓库
+import {reqCategoryList} from '@/api';
 
-const state = {};
-const mutations = {};
-const actions = {};
+// home模块小仓库
+const state = {
+    // state中数据默认初始值为空，根据接口的返回值
+    categoryList:[],
+};
+const mutations = {
+    CATEGORYLIST(state,categoryList){
+        state.categoryList = categoryList; 
+        state.categoryList = state.categoryList.slice(0,16); //多一个数据
+    }
+};
+const actions = {
+    // 通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async categoryList({commit}){
+        let result = await reqCategoryList();
+        if(result.code==200){
+            commit("CATEGORYLIST", result.data);
+        }
+    }
+};
 const getters = {};
 export default {
     state,
